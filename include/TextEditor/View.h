d833ee7967d9a07cbaced763f8574e9a603a902a
brshpl <igor_barishpol@mail.ru>
2020-12-10 03:00:18 +0300

    Implementing input and output

--- include/TextEditor/View.h
+++ include/TextEditor/View.h
@@ -7,19 +7,44 @@
 #include "Text.h"
 #include "TextEditor.h"
 
+enum editorKey {
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
+};
+
 class View {
 public:
     View();
     void moveCursor(Direction direction);
 
     TextEditor& getTextEditor();
-    void listen(int fd);
+    void listen(const int fd_in, const int fd_out);
 private:
     void show(const std::string& text);
     static void disableRawMode();
     static void enableRawMode();
+    void editorProcessKeypress(int fd);
+    int editorReadKey(int fd);
+    void editorRefreshScreen(int fd);
+    void editorDrawRows(int fd);
+    int getWindowSize(int *rows, int *cols);
 
-    static struct termios orig_termios;
+    void editorMoveCursor(int key);
+
+    static struct editorConfig {
+        static int cx, cy;
+        static int screenrows;
+        static int screencols;
+        static struct termios orig_termios;
+    };
+
     TextEditor textEditor_;
     Text text_;
 };
