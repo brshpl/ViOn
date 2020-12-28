1ac38d1f02d0839cb24b3314c261d59220a13a06
Vladimir Lunkin <vova.gold.201427@gmail.com>
2020-12-28 07:47:17 +0300

    1

--- ViOnClient/src/main.cpp
+++ ViOnClient/src/main.cpp
@@ -3,13 +3,14 @@
 
 
 int main(int argc, char* argv[]) {
-//    if (argc != 3) {
-//        return 1;
-//    }
+    if (argc != 3) {
+        return 1;
+    }
 //    int port = std::stoi(argv[2]);
 
-    ClientEditor editor(argv[1], 5555, "54.93.54.224");
+    ClientEditor editor(argv[1], 5555, argv[2]);
     editor.startEdit();
 
     return 0;
 }
+//"54.93.54.224"
